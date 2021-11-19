686bd6f31d015a5af2b48f061537f96308e1a5f0
oleksandr-babii <sancho_10_86@ukr.net>
2021-11-19 18:22:17 +0200

    #6 Form key. DB schema. Data persistence: created Model, ResourceModel, database table, saving data to the database

--- app/code/Oleksandrb/RegularCustomer/Controller/Index/Request.php
+++ app/code/Oleksandrb/RegularCustomer/Controller/Index/Request.php
@@ -4,6 +4,7 @@ declare(strict_types=1);
 
 namespace Oleksandrb\RegularCustomer\Controller\Index;
 
+use Oleksandrb\RegularCustomer\Model\DiscountRequest;
 use Magento\Framework\App\Request\InvalidRequestException;
 use Magento\Framework\App\RequestInterface;
 use Magento\Framework\Controller\Result\Redirect;
@@ -13,25 +14,73 @@ class Request implements
     \Magento\Framework\App\CsrfAwareActionInterface
 {
     /**
-     * @var \Magento\Framework\Controller\Result\RedirectFactory
+     * @var \Magento\Framework\Controller\Result\RedirectFactory $redirectFactory
      */
     private \Magento\Framework\Controller\Result\RedirectFactory $redirectFactory;
 
     /**
-     * @var \Magento\Framework\Message\ManagerInterface
+     * @var \Magento\Framework\Message\ManagerInterface $messageManager
      */
     private \Magento\Framework\Message\ManagerInterface $messageManager;
 
     /**
+     * @var \Oleksandrb\RegularCustomer\Model\DiscountRequestFactory $discountRequestFactory
+     */
+    private \Oleksandrb\RegularCustomer\Model\DiscountRequestFactory $discountRequestFactory;
+
+    /**
+     * @var \Oleksandrb\RegularCustomer\Model\ResourceModel\DiscountRequest $discountRequestResource
+     */
+    private \Oleksandrb\RegularCustomer\Model\ResourceModel\DiscountRequest $discountRequestResource;
+
+    /**
+     * @var \Magento\Framework\App\RequestInterface $request
+     */
+    private \Magento\Framework\App\RequestInterface $request;
+
+    /**
+     * @var \Magento\Store\Model\StoreManagerInterface $storeManager
+     */
+    private \Magento\Store\Model\StoreManagerInterface $storeManager;
+
+    /**
+     * @var \Magento\Framework\Data\Form\FormKey\Validator $formKeyValidator
+     */
+    private \Magento\Framework\Data\Form\FormKey\Validator $formKeyValidator;
+
+    /**
+     * @var \Psr\Log\LoggerInterface $logger
+     */
+    private \Psr\Log\LoggerInterface $logger;
+
+    /**
      * @param \Magento\Framework\Controller\Result\RedirectFactory $redirectFactory
      * @param \Magento\Framework\Message\ManagerInterface $messageManager
+     * @param \Oleksandrb\RegularCustomer\Model\DiscountRequestFactory $discountRequestFactory
+     * @param \Oleksandrb\RegularCustomer\Model\ResourceModel\DiscountRequest $discountRequestResource
+     * @param \Magento\Framework\App\RequestInterface $request
+     * @param \Magento\Store\Model\StoreManagerInterface $storeManager
+     * @param \Magento\Framework\Data\Form\FormKey\Validator $formKeyValidator
+     * @param \Psr\Log\LoggerInterface $logger
      */
     public function __construct(
         \Magento\Framework\Controller\Result\RedirectFactory $redirectFactory,
-        \Magento\Framework\Message\ManagerInterface $messageManager
+        \Magento\Framework\Message\ManagerInterface $messageManager,
+        \Oleksandrb\RegularCustomer\Model\DiscountRequestFactory $discountRequestFactory,
+        \Oleksandrb\RegularCustomer\Model\ResourceModel\DiscountRequest $discountRequestResource,
+        \Magento\Framework\App\RequestInterface $request,
+        \Magento\Store\Model\StoreManagerInterface $storeManager,
+        \Magento\Framework\Data\Form\FormKey\Validator $formKeyValidator,
+        \Psr\Log\LoggerInterface $logger
     ) {
         $this->redirectFactory = $redirectFactory;
         $this->messageManager = $messageManager;
+        $this->discountRequestFactory = $discountRequestFactory;
+        $this->discountRequestResource = $discountRequestResource;
+        $this->request = $request;
+        $this->storeManager = $storeManager;
+        $this->formKeyValidator = $formKeyValidator;
+        $this->logger = $logger;
     }
 
     /**
@@ -41,7 +90,26 @@ class Request implements
      */
     public function execute(): Redirect
     {
-        $this->messageManager->addSuccessMessage('Your request has been submitted');
+        /** @var DiscountRequest $discountRequest */
+        $discountRequest = $this->discountRequestFactory->create();
+
+        try {
+            $discountRequest->setProductId((int) $this->request->getParam('product_id'))
+                ->setName($this->request->getParam('name'))
+                ->setEmail($this->request->getParam('email'))
+                ->setAdminUserId($this->request->getParam(''))
+                ->setStoreId($this->storeManager->getStore()->getId());
+
+            $this->discountRequestResource->save($discountRequest);
+            $this->messageManager->addSuccessMessage(
+                __('You request for product %1 accepted for review!', $this->request->getParam('productName'))
+            );
+        } catch (\Exception $e) {
+            $this->logger->error($e->getMessage());
+            $this->messageManager->addErrorMessage(
+                __('Your request can\'t be sent. Please, contact us if you see this message.')
+            );
+        }
 
         $redirect = $this->redirectFactory->create();
         $redirect->setRefererUrl();
@@ -64,10 +132,10 @@ class Request implements
      * Perform custom request validation. Return null if default validation is needed.
      *
      * @param RequestInterface $request
-     * @return bool|null
+     * @return bool
      */
-    public function validateForCsrf(RequestInterface $request): ?bool
+    public function validateForCsrf(RequestInterface $request): bool
     {
-        return null;
+        return $this->formKeyValidator->validate($request);
     }
 }
