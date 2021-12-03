73809b9ad4736b3cf9e28b474bf34fc9a0f207a9
oleksandr-babii <sancho_10_86@ukr.net>
2021-12-03 21:06:04 +0200

    Lesson 8 - Magento upgrade and going live: Upgraded Magento to 2.4.3-p1

--- app/code/Oleksandrb/RegularCustomer/Block/Account/SortTopLink.php
+++ app/code/Oleksandrb/RegularCustomer/Block/Account/SortTopLink.php
@@ -6,7 +6,7 @@ namespace Oleksandrb\RegularCustomer\Block\Account;
 
 use Magento\Customer\Block\Account\SortLinkInterface;
 
-class SortTopLink extends \Magento\Framework\View\Element\Html\Link\Current implements SortLinkInterface
+class SortTopLink extends \Magento\Framework\View\Element\Html\Link implements SortLinkInterface
 {
     /**
      * @inheritdoc
